a444431fd0070866cc4c45a488cb8a0faff3a941
Ghizlane <zouhareghizlane@outlook.com>
2016-10-02 16:24:43 +0100

    Add project files.

---
+++ Memberships/Entities/Section.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Memberships.Entities
+{
+    [Table("Section")]
+    public class Section
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [MaxLength(255)]
+        [Required]
+        public String Title { get; set; }
+    }
+}
